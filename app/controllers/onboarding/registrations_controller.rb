10abff28cb8057ce9b5e5dc15a39d357571142cd
Sam Spence <sam@nayya.com>
2022-05-18 12:21:37 -0600

    initial monad + matcher setup from tutorial

---
+++ app/controllers/onboarding/registrations_controller.rb
@@ -0,0 +1,13 @@
+module Onboarding
+  class RegistrationsController < ApplicationController
+    def create
+      call_action(create_user)
+    end
+
+    private
+
+    def create_user
+      Endpoints::CreateUser::Action.new
+    end
+  end
+end
---
+++ lib/onboarding/endpoints/create_user/action.rb
@@ -0,0 +1,33 @@
+require 'dry/monads'
+require 'dry/matcher/result_matcher'
+
+module Onboarding
+  module Endpoints
+    module CreateUser
+      class Action
+        include Dry::Monads[:do, :result]
+        include Dry::Matcher.for(:call, with: Dry::Matcher::ResultMatcher)
+
+        def call(request)
+          model, auth = yield deserializer.call(request)
+          yield authorizer.call(model, auth)
+          res = yield validator.call(model.to_h)
+          yield create_user.call(res.to_h)
+
+          Success(status: :created)
+        end
+
+        private
+
+        attr_reader :deserializer, :authorizer, :validator, :create_user
+
+        def initialize
+          @authorizer = Authorizer.new
+          @deserializer = Deserializer.new
+          @validator = Validator.new(repository: User)
+          @create_user = Operations::CreateUser.new
+        end
+      end
+    end
+  end
+end
